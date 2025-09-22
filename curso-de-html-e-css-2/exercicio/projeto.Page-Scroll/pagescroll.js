2f4cac171f3aa3bdf902dce43fa90de26d677fc6
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-09-22 14:52:08 -0300

    feito

---
+++ curso-de-html-e-css-2/exercicio/projeto.Page-Scroll/pagescroll.js
@@ -0,0 +1,23 @@
+function azul(){
+   let azul = document.getElementById('s1');
+
+    azul.style.backgroundColor = '#1dbcb1'
+} 
+
+function verde(){
+   let verder = document.getElementById('s2');
+
+    verder.style.backgroundColor = '#0f700e'
+} 
+
+function amarelo(){
+   let amarelo = document.getElementById('s3');
+
+    amarelo.style.backgroundColor = '#ffff69'
+} 
+
+function violeta(){
+   let violeta = document.getElementById('s4');
+
+    violeta.style.backgroundColor = '#4c3b7c'
+} 
