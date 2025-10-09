0e249971e8f014cb84bd028b1c5912f46794d37a
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-10-09 15:24:16 -0300

    menu3 save

---
+++ arquivo-teste/teste-menu3/menu.js
@@ -0,0 +1,9 @@
+let nav = document.getElementById('nav')
+
+function Menu(){
+    if(nav.style.display == 'block'){
+        nav.style.display = 'none'
+    }else{
+        nav.style.display = 'block'
+    }
+}
