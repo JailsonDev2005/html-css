c1c981f29ef3ed5dc1ff8a32bdb4d5b41887f106
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-10-08 21:23:20 -0300

    teste menu save

---
+++ arquivo-teste/teste-menu/menu.js
@@ -0,0 +1,10 @@
+let Nav = document.getElementById("nav");
+
+
+function Menu(){
+    if(Nav.style.display == 'flex'){
+        Nav.style.display = 'none'
+    } else {
+        Nav.style.display = 'flex'
+    }
+}
