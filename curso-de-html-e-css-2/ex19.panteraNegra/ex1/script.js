8b79eaf4e2403731617c0d94a59795ede51eddb2
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-10-23 17:10:39 -0300

    estudo mais ou menos

---
+++ curso-de-html-e-css-2/ex19.panteraNegra/ex1/script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById('menu-btn');
+const navBox = document.getElementById('nav-box');
+
+menu.addEventListener("click", () => {
+    navBox.classList.toggle('active')
+})
