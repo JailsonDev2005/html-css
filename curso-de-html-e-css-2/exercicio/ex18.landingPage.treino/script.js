1355a21f646db77068b1b9b6da79f40354008d73
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-10-24 19:23:46 -0300

    desistir

--- curso-de-html-e-css-2/exercicio/ex18.landingPage.treino/script.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const nav = document.getElementById('nav');
-
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    const icon = menuToggle.querySelector('i');
-    icon.classList.toggle('fa-bars');
-    icon.classList.toggle('fa-xmark'); // ícone de fechar
-});
