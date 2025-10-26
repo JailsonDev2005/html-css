7ef968cb01373080cf9514379b845e5fa7b8912a
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-10-25 21:13:09 -0300

    menu save

---
+++ teste/teste3/script.js
@@ -0,0 +1,13 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+
+    sidebar.style.display = "flex"
+
+    
+}
+
+function hiderSidebar() {
+     const closebar = document.querySelector('.sidebar');
+
+    closebar.style.display = "none"
+}
