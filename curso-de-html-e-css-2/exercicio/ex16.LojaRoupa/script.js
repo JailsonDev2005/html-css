04fb497578937e82591f14d8ce9b08e12cc57dba
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-10-26 23:54:59 -0300

    menu projeto loja finalizado

---
+++ curso-de-html-e-css-2/exercicio/ex16.LojaRoupa/script.js
@@ -0,0 +1,15 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navlinks = document.querySelector('.nav-links')
+const icon = document.getElementById('i')
+
+menuToggle.addEventListener('click', ()=>{
+    navlinks.classList.toggle('mobile-menu')
+
+    if(navlinks.classList.contains('mobile-menu')){
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-xmark')
+    }else{
+        icon.classList.remove('fa-xmark');
+        icon.classList.add('fa-bars')
+    }
+})
