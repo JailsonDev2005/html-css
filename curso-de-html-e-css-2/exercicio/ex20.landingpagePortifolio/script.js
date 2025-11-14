3c696d12cb9f490eddde2a31558bb838f04e308b
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-11-13 21:25:46 -0300

    projeto portfolio finalizado

--- curso-de-html-e-css-2/exercicio/ex20.landingpagePortifolio/script.js
+++ curso-de-html-e-css-2/exercicio/ex20.landingpagePortifolio/script.js
@@ -5,3 +5,35 @@ let menuI = menuH.querySelector("i");
 menuH.addEventListener("click", ()=>{
  navLink.classList.toggle("open")
 })
+
+const ScrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 2000,
+}
+
+ScrollReveal().reveal(".principal .img img",{
+    ...ScrollRevealOption,
+    delay: 3500,
+})
+
+ScrollReveal().reveal(".principal .conteudo h3",{
+    ...ScrollRevealOption,
+    delay: 1000,
+})
+
+ScrollReveal().reveal(".principal .conteudo h2",{
+    ...ScrollRevealOption,
+    delay: 2500,
+})
+
+ScrollReveal().reveal(".principal .conteudo p",{
+    ...ScrollRevealOption,
+    delay: 3000,
+})
+
+ScrollReveal().reveal(".principal .conteudo .mainBtn",{
+    ...ScrollRevealOption,
+    delay: 3500,
+    interval: 500,
+})
