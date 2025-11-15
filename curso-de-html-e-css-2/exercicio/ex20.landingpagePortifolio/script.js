f7e2ee165680bf8f5c79d9d3ce11f5185f35b32e
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-11-15 15:28:36 -0300

    btn nav bar concluido

--- curso-de-html-e-css-2/exercicio/ex20.landingpagePortifolio/script.js
+++ curso-de-html-e-css-2/exercicio/ex20.landingpagePortifolio/script.js
@@ -1,10 +1,21 @@
-let menuH = document.querySelector(".menu-link");
-let navLink = document.querySelector(".nav-bar");
+let menuH = document.querySelector(".btnMenu");
+let navLink = document.querySelector("ul");
 let menuI = menuH.querySelector("i");
 
 menuH.addEventListener("click", ()=>{
  navLink.classList.toggle("open")
-})
+
+ let isOpen = navLink.classList.contains("open");
+ menuI.setAttribute(
+    "class",
+    isOpen ? "fa-solid fa-x" : "fa-solid fa-bars"
+ );
+});
+
+navLink.addEventListener("click", ()=> {
+    navLink.classList.remove("open");
+    menuH.setAttribute("class", "fa-solid fa-x")
+});
 
 const ScrollRevealOption = {
     distance: "50px",
