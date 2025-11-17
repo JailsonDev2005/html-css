100eafb0ac45f9e2dd898cefae0c4470870a7e5d
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-11-17 01:06:19 -0300

    Estrutura projeto reacr

--- projeto-React/costs/src/App.js
+++ projeto-React/costs/src/App.js
@@ -1,10 +1,32 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/page/Home';
+import Contact from './components/page/Contact';
+import Company from './components/page/Company';
+import NewProject from './components/page/NewProjeto';
 
+import Container from './components/layout/Container';
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+          <Link to="/">Home</Link>
+          <Link to="/contacty">Contatos</Link>
+          <Link to="/company">Company</Link>
+          <Link to="/newproject">Novo Projeto</Link>
+      </ul>
+      <Container customClass="min-height">
+          <Routes>
+        
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} /> 
+          <Route path="/contacty" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        
+          </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
