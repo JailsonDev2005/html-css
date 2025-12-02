970a1c39610d8007e3da9b3551a9cb192b2b9d63
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-12-02 15:35:38 -0300

    concluido

---
+++ css-grid/grid-1/script.js
@@ -0,0 +1,8 @@
+const sidebar = document.getElementById("sidebar");
+
+const btn = document.querySelector('button');
+
+
+btn.addEventListener("click", ()=>{
+    sidebar.classList.toggle('show')
+})
