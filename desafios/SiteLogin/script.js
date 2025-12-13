31f63ef297e977f21bad838ac42a01ff0706713d
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-12-13 12:34:08 -0300

    ajustando

--- desafios/SiteLogin/script.js
+++ desafios/SiteLogin/script.js
@@ -2,16 +2,19 @@ let btnEntrar = document.getElementById("entrar");
 let cadastro = document.getElementById("cadastro");
 let login = document.getElementById("login");
 let btnCadastro = document.getElementById("btnCadastro");
-let logincont = document.getElementById("EntLogin");
-let container = document.querySelector(".form-container");
 
 
-function mudalogin(){
-    if(cadastro.classList.contains){
-    cadastro.classList.add("active")
-    login.classList.add("close")
-    }else{
-        login.classList.add("active")
-        cadastro.classList.remove("active")
+
+btnEntrar.addEventListener("click", ()=>{
+    if(cadastro.style.display = "flex"){
+        cadastro.style.display = "none"
+        login.style.display = "flex"
     }
-}
+})
+
+ btnCadastro.addEventListener("click", ()=>{
+    if(cadastro.style.display = "none"){
+        cadastro.style.display = "flex"
+        login.style.display = "none"
+    }
+})
