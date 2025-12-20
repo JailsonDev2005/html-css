688bc6f39ca375db6c9fd8ab0334acf6d2d28391
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-12-19 23:11:39 -0300

    site concluido

--- curso-de-html-e-css-2/exercicio/ex20.landingpagePortifolio/script.js
+++ curso-de-html-e-css-2/exercicio/ex20.landingpagePortifolio/script.js
@@ -1,50 +1,19 @@
-let menuH = document.querySelector(".btnMenu");
-let navLink = document.querySelector("ul");
-let menuI = menuH.querySelector("i");
+let btnLinks = document.querySelector(".btnLinks");
+let navegacao = document.querySelector(".navegacao");
+let icon = btnLinks.querySelector("i");
+let link = navegacao.querySelectorAll("a")
 
-menuH.addEventListener("click", ()=>{
- navLink.classList.toggle("open")
 
- let isOpen = navLink.classList.contains("open");
- menuI.setAttribute(
-    "class",
-    isOpen ? "fa-solid fa-x" : "fa-solid fa-bars"
- );
-});
-
-navLink.addEventListener("click", ()=> {
-    navLink.classList.remove("open");
-    menuI.setAttribute("class", "fa-solid fa-bars")
-});
-
-const ScrollRevealOption = {
-    distance: "50px",
-    origin: "bottom",
-    duration: 2000,
-}
-
-ScrollReveal().reveal(".imagen img",{
-    ...ScrollRevealOption,
-    delay: 1000,
+btnLinks.addEventListener("click", ()=>{
+    navegacao.classList.toggle("open")
+    icon.classList.toggle("fa-xmark")
+    icon.classList.toggle("fa-bars")
 })
 
-ScrollReveal().reveal(".conteudo h3",{
-    ...ScrollRevealOption,
-    delay: 500,
-})
-
-ScrollReveal().reveal(".conteudo h2",{
-    ...ScrollRevealOption,
-    delay: 1000,
-})
-
-ScrollReveal().reveal(".conteudo p",{
-    ...ScrollRevealOption,
-    delay: 1500,
-})
-
-ScrollReveal().reveal(".conteudo .btn",{
-    ...ScrollRevealOption,
-    delay: 2000,
-    interval: 500,
+link.forEach(link => {
+    link.addEventListener("click", ()=>{
+        navegacao.classList.toggle("open")
+        icon.classList.toggle("fa-xmark")
+        icon.classList.toggle("fa-bars")
+    })
 })
