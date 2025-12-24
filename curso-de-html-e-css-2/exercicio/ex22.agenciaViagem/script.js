d2859dc452668e0b48772e29e43f95dc1b700c17
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-12-24 17:29:15 -0300

    em andamento

--- curso-de-html-e-css-2/exercicio/ex22.agenciaViagem/script.js
+++ curso-de-html-e-css-2/exercicio/ex22.agenciaViagem/script.js
@@ -1,3 +1,29 @@
+let btnNav = document.querySelector(".btnBars");
+let nav = document.querySelector(".nav");
+let btnIcon = btnNav.querySelector("i");
+let link = nav.querySelectorAll("a");
+
+btnNav.addEventListener("click", ()=>{
+    nav.classList.toggle("open")
+        btnIcon.classList.toggle("fa-bars")
+        btnIcon.classList.toggle("fa-xmark")
+})
+
+
+link.forEach(link =>{
+    link.addEventListener("click", ()=>{
+        nav.classList.remove("open")
+        btnIcon.classList.add("fa-bars")
+        btnIcon.classList.remove("fa-xmark")
+    })
+})
+
+
+
+
+
+
+
 window.addEventListener("scroll", () => {
     const header = document.querySelector(".header");
     header.classList.toggle("sticky", window.scrollY > 0);
